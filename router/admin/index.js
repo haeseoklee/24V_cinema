89a31689a176252d8cfb73840a8dedb3e757bfa4
Hee-Jae <jhj967878@naver.com>
2019-11-16 16:38:42 +0900

    admin.ejs update

--- router/admin/index.js
+++ router/admin/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function(req, res){
-    res.render();
+   
+    res.render('admin.ejs', {});
 });
 
 module.exports = router;
