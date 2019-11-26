e6572e998c513a7ec1a5cadc592a8cdb89aa6831
haeseoklee <haeseoklee.dev@gmail.com>
2019-11-26 16:34:11 +0900

    admin 로그인 기능 수정, ticket 예약시 로그인 했는지 검사

--- router/admin/index.js
+++ router/admin/index.js
@@ -3,31 +3,27 @@ var router = express.Router();
 var db = require('../../db/db');
 
 
-router.get('/', function(req, res){
-    getOnlyAdmin(req, res);
-    
-});
-
 router.get('/console', function(req, res){
-    getOnlyAdmin(req, res);
+    isAdmin(req, res, function(){
+        res.render('admin_console.ejs');
+    });
 })
 
-function getOnlyAdmin(req, res){
-    if (!req.isAuthenticated()) {
-        res.render('admin_login.ejs');
-    } else {
+function isAdmin(req, res, fn){
+    if (req.isAuthenticated()) {
         var sql = 'select email from customer where id=?';
         var query = db.query(sql, [req.user], function(err, rows){
             var data = JSON.parse(JSON.stringify(rows));
             if(err) res.redirect('/');
-            if (data[0].email === 'admin'){
-                res.redirect('/admin/console');
+            if (data[0].email === 'admin@admin.com'){
+                fn();
             }else{
                 res.redirect('/');
             }
         })
+    } else {
+        res.redirect('/user/login');
     }   
 }
-
 
 module.exports = router;
