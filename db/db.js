183f3bf81e882a714391582ad027695fd25c3ad5
haeseoklee <haeseoklee.dev@gmail.com>
2019-11-30 21:26:18 +0900

    .

--- db/db.js
+++ db/db.js
@@ -2,10 +2,11 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host : 'localhost',
-    port: 3306,
+    port: 8888,
     user : 'root',
-    password : 'satigofk12',
-    database : '24V_cinema'
+    password : 'root',
+    database : '24V_cinema',
+    socketPath :'/Applications/MAMP/tmp/mysql/mysql.sock',
 });
 connection.connect();
 
