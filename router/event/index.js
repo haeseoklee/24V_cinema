07d9c1dbcabaed2ab28815bd8fa4e768bddb960b
Hee-Jae <jhj967878@naver.com>
2019-11-30 21:22:22 +0900

    메인 페이지 디자인, 이벤트 페이지 추가

--- router/event/index.js
+++ router/event/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('timetable.ejs');
+    res.render('event.ejs');
 });
 
 module.exports = router;
