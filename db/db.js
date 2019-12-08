3798f527fb39763f5862b5c252dabb7efa842769
Hee-Jae <jhj967878@naver.com>
2019-12-08 15:08:57 +0900

    db.js changed

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host : 'localhost',
     port: 3306,
     user : 'root',
-    password : 'satigofk12',
+    password : 'root',
     database : '24V_cinema'
 });
 connection.connect();
