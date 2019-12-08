7a2450e908c3062f9fd93de19f09ef12089a4e93
hsh04060 <hsh04060@naver.com>
2019-12-08 18:23:40 +0900

    db.js ......

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host : 'localhost',
     port: 3306,
     user : 'root',
-    password : 'root',
+    password : 'asd890',
     database : '24V_cinema'
 });
 connection.connect();
